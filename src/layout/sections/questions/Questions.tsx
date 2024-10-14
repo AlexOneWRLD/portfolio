17fb08564115d798fe44188d92c0f89e2e59468b
Aleksandr Ovsyannikov <116282471+AlexOneWRLD@users.noreply.github.com>
2024-10-14 17:51:15 +0300

    add style questions

--- src/layout/sections/questions/Questions.tsx
+++ src/layout/sections/questions/Questions.tsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
+import { ColorText } from '../../../components/styles-components/colortext/ColorText'
 
 export const Questions = () => {
 	return (
 		<QuestionsSection>
 			<Text>For any questions please mail me:</Text>
-			<Link href={'mailto:a.v.ovsyannikov@internet.ru'}>a.v.ovsyannikov@internet.ru</Link>
+			<Link href={'mailto:a.v.ovsyannikov@internet.ru'}><ColorText>a.v.ovsyannikov@internet.ru</ColorText></Link>
 		</QuestionsSection>
 	)
 }
 
 
-const QuestionsSection = styled.div `
+const QuestionsSection = styled.div`
   margin: 0 auto 205px;
 `
 
-const  Text = styled.p `
-  padding: 0 179px;
+const Text = styled.p`
+  text-align: center;
   font-weight: 700;
   font-size: 58px;
   line-height: 1.2069;
@@ -24,14 +25,10 @@ const  Text = styled.p `
   color: #ccc;
 `
 
-const Link = styled.a `
-font-weight: 700;
-font-size: 58px;
-line-height: 1.2069;
-letter-spacing: -0.02em;
-text-align: center;
-background: linear-gradient(90deg, #13b0f5 2.6%, #e70faa 100%);
-background-clip: text;
-	text-decoration: none;
-	padding: 0 251px;
+const Link = styled.a`
+  font-weight: 700;
+  font-size: 58px;
+  line-height: 1.2069;
+  letter-spacing: -0.02em;
+	padding: 0 180px;
 `
