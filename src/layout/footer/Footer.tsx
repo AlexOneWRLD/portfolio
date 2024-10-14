3824b153c1420f69adca130352c077f82ee62e23
Aleksandr Ovsyannikov <116282471+AlexOneWRLD@users.noreply.github.com>
2024-10-14 18:05:52 +0300

    add styled footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -3,21 +3,24 @@ import styled from 'styled-components'
 import { Logo } from '../../components/logo/Logo'
 import { Menu } from '../../components/menu/Menu'
 import { Social } from '../../components/social/Social'
+import { ColorText } from '../../components/styles-components/colortext/ColorText'
 import { FlexWrapper } from '../../components/styles-components/flexwrapper/FlexWrapper'
 
 const footerMenu = ['Home', 'About', 'Technologies', 'Projects', 'Contact']
 export const Footer = () => {
 	return (
 		<FooterSection>
-			<FlexWrapper align={'center'} padding={'0 0 50px 0'} gap={'35px'} justify={'space-between'} borderbt={'2px solid #42446e'}>
+			<FlexWrapper align={'center'} padding={'0 0 50px 0'} gap={'100px'} justify={'space-between'} borderbt={'2px solid #42446e'}>
 				<Logo />
+				<FlexWrapper gap={'50px'}>
 				<a href={'tel:+911234509876'}>+91 12345 09876</a>
 				<a href={'mailto:a.v.ovsyannikov@internet.ru'}>a.v.ovsyannikov@internet.ru</a>
+				</FlexWrapper>
 				<Social />
 			</FlexWrapper>
 			<FlexWrapper align={'center'} padding={'50px 0 0 0'} gap={'35px'} justify={'space-between'}>
 				<Menu menuItems={footerMenu}/>
-				<Copy>Designed and built by Pavan MG with Love & Coffee</Copy>
+				<Copy>Designed and built by <ColorText>Ovsyannikov AV</ColorText> with <ColorText>Love</ColorText>  & <ColorText>Coffee</ColorText></Copy>
 			</FlexWrapper>
 		</FooterSection>
 	)
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { Logo } from '../../components/logo/Logo'
 import { Menu } from '../../components/menu/Menu'
+import { Social } from '../../components/social/Social'
 import { Container } from '../../components/styles-components/container/Container'
 import { FlexWrapper } from '../../components/styles-components/flexwrapper/FlexWrapper'
 
@@ -10,9 +11,12 @@ export const Header = () => {
 	return (
 		<StyledHeader>
 			<Container>
-				<FlexWrapper justify={'space-between'} align={'center'}>
+				<FlexWrapper align={'center'}>
 					<Logo />
-					<Menu menuItems={headerMenu} ></Menu>
+					<FlexWrapper gap={'51px'}>
+						<Menu menuItems={headerMenu}></Menu>
+						<Social />
+					</FlexWrapper>
 				</FlexWrapper>
 			</Container>
 		</StyledHeader>
@@ -21,8 +25,8 @@ export const Header = () => {
 
 const StyledHeader = styled.header`
   padding: 40px 0 0 0;
-	position: fixed;
-	top: 0;
-	right: 0;
-	left: 0;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
 `
