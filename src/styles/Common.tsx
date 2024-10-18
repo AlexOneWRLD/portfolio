733daabc848ab6f3999817f8c55d0bde632d9239
Aleksandr Ovsyannikov <116282471+AlexOneWRLD@users.noreply.github.com>
2024-10-18 13:09:54 +0300

    pull

--- src/styles/Common.tsx
+++ src/styles/Common.tsx
@@ -5,13 +5,13 @@ type FontPropsType = {
 	weight?: number
 	color?: string
 	lineHeight?: number
-	 Fmin?: number
+	Fmin?: number
 	Fmax?: number
 }
-export const font = ({family, weight,color,lineHeight,Fmin,Fmax}: FontPropsType) =>`
-	font-family: ${family || 'Poppins' };
-	font-weight: ${weight || '400' };
-	color: ${color || theme.colors.font };
-	line-height: ${lineHeight || '1.3' };
+export const font = ({ family, weight, color, lineHeight, Fmin, Fmax }: FontPropsType) => `
+	font-family: ${family || 'Poppins'};
+	font-weight: ${weight || '400'};
+	color: ${color || theme.colors.font};
+	line-height: ${lineHeight || '1.3'};
 	font-size: calc((100vw - 320px) / (1600 - 320) * (${Fmax} - ${Fmin}) + ${Fmin}px);
 `
